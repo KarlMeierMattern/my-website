a0ba90b48c144dfe25f8ee181e2b45c0ca3b248b
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2024-10-05 21:08:39 +0200

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Card from "@/components/card";
+import profilePic from "./public/images/profilePic.png"
 
 export default function Home() {
   return (
@@ -7,7 +8,13 @@ export default function Home() {
       <div className="row-start-1 col-start-1 gap-8 items-center sm:items-start">
         <div>
           <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500 text-5xl">Welcome</h1>
-          {/* <Image></Image> */}
+          <Image
+            className="rounded-3xl m-16"
+            src={profilePic}
+            width={300}
+            height={300}
+            alt="Picture of the author"
+          />
           {/* <Button></Button> */}
         </div>
       </div>
