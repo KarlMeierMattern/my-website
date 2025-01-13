d897eb995a35cd9969d3fce3d24838affacdb3bb
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-01-13 20:14:37 +0200

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,48 +7,38 @@ import landscape from "@/app/public/images/landscape.png";
 import alsoMe from "@/app/public/images/alsoMe.png";
 import { useState, useEffect } from "react";
 import Contact from "@/components/navigation/sections/Contact";
-// import Arrow1 from "@/components/arrow";
 
 export default function Home() {
   const [section, setSection] = useState("");
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsLargeScreen(window.innerWidth >= 768); // Use a breakpoint (e.g., 768px)
-    };
-
-    handleResize(); // Initialize on mount
-    window.addEventListener("resize", handleResize); // Update on resize
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
 
   return (
     <main className="bg-zinc-50 min-h-screen">
       <section className=" bg-zinc-300  w-screen h-screen">
-        <div className="fixed top-4 left-0 right-0 w-screen z-10 flex justify-center">
-          <Navbar
-            setSection={setSection}
-            selectedSection={section}
-            isLargeScreen={isLargeScreen}
+        <div className="fixed top-4 left-0 right-0 w-screen flex justify-center z-20">
+          <Navbar setSection={setSection} selectedSection={section} />
+        </div>
+        <div className="z-10 md:w-full md:h-screen">
+          <Image
+            className="md:object-cover md:opacity-90 opacity-0 w-full h-full"
+            src={landscape}
+            alt="Landscape picture"
           />
         </div>
-        <Image
-          className={`absolute bottom-0 right-0 z-20 opacity-80 md:w-screen md:h-screen md:object-cover md:opacity-90 md:z-0 w-3/4`}
-          src={isLargeScreen ? landscape : alsoMe}
-          alt="Landscape picture"
-        />
-        {!isLargeScreen && (
+        <div className="md:hidden">
+          <Image
+            className="absolute bottom-0 right-0 w-3/4 z-20 opacity-80"
+            src={alsoMe}
+            alt="Alternate picture"
+          />
           <div>
             <p className="absolute bottom-20 left-4 text-6xl font-sans italic opacity-60 text-white font-bold z-30">
               karl
             </p>
             <p className="absolute bottom-4 left-16 text-6xl font-sans opacity-90 text-white font-bold z-30">
               alexander
-            </p>{" "}
+            </p>
           </div>
-        )}
+        </div>
       </section>
       <section className="md:grid md:grid-cols-[0.5fr_1fr_0.5fr]">
         <div className="col-start-2">
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -5,23 +5,14 @@ import Button from "@/components/navigation/Button";
 type NavbarProps = {
   setSection: (section: string) => void;
   selectedSection: string;
-  isLargeScreen: boolean;
 };
 
-export default function Navbar({
-  setSection,
-  selectedSection,
-  isLargeScreen,
-}: NavbarProps) {
+export default function Navbar({ setSection, selectedSection }: NavbarProps) {
   const navbarOptions = ["about", "portfolio", "resume", "library", "contact"];
 
   return (
     <div>
-      <nav
-        className={`bg-zinc-400 bg-opacity-40 backdrop-blur-sm mx-auto ${
-          isLargeScreen ? "rounded-xl p-1" : "rounded-lg p-1"
-        }`}
-      >
+      <nav className="bg-zinc-400 bg-opacity-40 backdrop-blur-sm mx-auto rounded-lg p-1 md:rounded-xl md:p-1">
         {navbarOptions.map((option) => (
           <Button
             isSelected={selectedSection === option}
